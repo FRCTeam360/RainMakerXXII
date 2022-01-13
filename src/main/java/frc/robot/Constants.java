8377493e7bc28dda850d7afe0872c74d3d2869b8
lauren-lai <25lailauren@bprep.org>
2022-01-12 17:28:45 -0800

    imported descriptions from testdrivebase constants,

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,29 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    
+    public static final class DriveTrainConstants {
+        public static final int motorLLeadID = 1;
+        public static final int motorLFollow1ID = 2;
+        public static final int motorLFollow2ID = 3;
+        public static final int motorRLeadID = 4;
+        public static final int motorRFollow1ID = 5;
+        public static final int motorRFollow2ID = 6;
+    }
+
+    public static final class OIConstants {
+        public static final int contPort = 0; // port of xbox controller connected
+        public static final int driverContPort = 1; 
+        public static final double xboxDeadzone = .125;
+    }
+
+    public static final class AutoConstants {
+        //Conversions for the Falcons
+        private static final double pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
+        //public static final double ticksToMeters = ( ((15.0/85.0)*(30.0/40.0)) / 1.0 ) * ( (pi * .1524) / 1.0 ); 
+        public static final double ticksToMeters = ( ((15.0/85.0)*(30.0/40.0)) / 1.0 ) * ( (pi * .1524) / 1.0 );
+        public static final boolean kGyroReversed = true; //Characterization says this isn't necessary but it seems to perform better....
+    }
+}
+
