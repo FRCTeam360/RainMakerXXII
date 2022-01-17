a92b6062406ab1e4d71a559f504b850f588c30fc
tbmilliren <60276487+tbmilliren@users.noreply.github.com>
2022-01-17 12:41:23 -0800

    Actually started the subsystem

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -4,10 +4,16 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
-  /** Creates a new Limelight. */
+
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry tv = table.getEntry("tv");
+  NetworkTableEntry tx = table.getEntry("tx");
+
   public Limelight() {}
 
   @Override
