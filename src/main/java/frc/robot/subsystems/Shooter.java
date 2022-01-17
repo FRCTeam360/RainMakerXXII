c5fac1169c630754cbfa30bedf1907881316dadb
een39 <24stoddardian@bprep.org>
2022-01-17 14:06:59 -0800

    Update Shooter.java

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -6,8 +6,10 @@ package frc.robot.subsystems;
 
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 
 
 import static frc.robot.Constants.ShooterConstants.*;
@@ -18,13 +20,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
 
+  private static final String kFF = null;
   private CANSparkMax shooterLead;
   private CANSparkMax shooterFollow;
+  private SparkMaxPIDController shooterPidController;
+
 
     public Shooter() {
         shooterLead = new CANSparkMax(shooterLeadId, MotorType.kBrushless);
         shooterFollow = new CANSparkMax(shooterFollowId, MotorType.kBrushless);
-    
+
+        shooterPidController = shooterLead.getPIDController();
+
         shooterLead.restoreFactoryDefaults();
         shooterFollow.restoreFactoryDefaults();
     
@@ -37,24 +44,23 @@ public class Shooter extends SubsystemBase {
     
         //shooterLead.setSensorPhase(true); //the Follower isn't harvested for it's encoder therefor rotation doesn't need to be modified
     
-        // set PID coefficients
-        //shooterLead.setP(kP, 0);
-        //shooterLead.setI(kI, 0);
-        //shooterLead.setD(kD, 0);
-        //shooterLead.setIZone(kIz);
-        //shooterLead.setFF(kFF);
-        //shooterLead.setOutputRange(kMinOutput, kMaxOutput);
-         //}
-        
-         shooterLead.configNominalOutputForward( 0 , kTimeOutMs);
-         shooterLead.configNominalOutputReverse( 0 , kTimeOutMs);
-         shooterLead.configPeakOutputForward( 1 , kTimeOutMs);
-         shooterLead.configPeakOutputReverse( -1 , kTimeOutMs);
-     
-         shooterLead.config_kF(kPIDLoopIdx, kF , kTimeOutMs );
-         shooterLead.config_kP(kPIDLoopIdx, kP , kTimeOutMs );
-         shooterLead.config_kI(kPIDLoopIdx, kI , kTimeOutMs );
-         shooterLead.config_kD(kPIDLoopIdx, kD , kTimeOutMs );
+        //set PID coefficients
+        shooterPidController.setP(kP, 0);
+        shooterPidController.setI(kI, 0);
+        shooterPidController.setD(kD, 0);
+        shooterPidController.setFF(kF);
+        shooterPidController.setOutputRange(kMin.Output, kMax.Output); //first kMIN.Input -> originally = kMin.Output
         }}
+        
+         //shooterLead.configNominalOutputForward( 0 , kTimeOutMs);
+         //shooterLead.configNominalOutputReverse( 0 , kTimeOutMs);
+         //shooterLead.configPeakOutputForward( 1 , kTimeOutMs);
+         //shooterLead.configPeakOutputReverse( -1 , kTimeOutMs);
+     
+         //shooterLead.config_kF(kPIDLoopIdx, kF , kTimeOutMs );
+         //shooterLead.config_kP(kPIDLoopIdx, kP , kTimeOutMs );
+         //shooterLead.config_kI(kPIDLoopIdx, kI , kTimeOutMs );
+         //shooterLead.config_kD(kPIDLoopIdx, kD , kTimeOutMs );
+        //}}
 
         
