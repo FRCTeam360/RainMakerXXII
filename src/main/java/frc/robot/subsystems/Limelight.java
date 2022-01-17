2d2a7e12aaa931cb46f27e3a0a3400dc1ccff78d
Katie-jpg <23Sukatie@bprep.org>
2022-01-17 12:58:05 -0800

    updated limelight

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -13,8 +13,51 @@ public class Limelight extends SubsystemBase {
   NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
   NetworkTableEntry tv = table.getEntry("tv");
   NetworkTableEntry tx = table.getEntry("tx");
+  NetworkTableEntry ty = table.getEntry("ty");
+  NetworkTableEntry ta = table.getEntry("ta");
+
+  NetworkTableEntry camMode = table.getEntry("camMode");
 
   public Limelight() {}
+
+  public void changeCamMode() {
+    if(camMode.getDouble(0.0) == 1.0){
+      table.getEntry("camMode").setNumber(0.0);
+      table.getEntry("ledMode").setNumber(1);
+    } else {
+      table.getEntry("camMode").setNumber(1.0);
+      table.getEntry("ledMode").setNumber(1);
+    }
+  }
+
+  public double getCamMode() {
+    return camMode.getDouble(0.0);
+  }
+
+  public boolean validTarget() {
+    return tv.getDouble(0.0) == 1.0;
+  }
+
+  public double getX() {
+    return tx.getDouble(0.0);
+  }
+
+  public double getY() {
+    return ty.getDouble(0.0);
+  }
+
+  public double getArea() {
+    return ta.getDouble(0.0);
+  }
+
+  // public void updateShooterVelocity () {
+  //   if ( validTarget() == true ) {
+  //     double yLime = getY();
+  //     targetVelocity = (aVal * yLime * yLime) + ( +bVal * yLime) + ( cVal ) ;
+  //   } else {
+  //     targetVelocity = backupTargetVelocity;
+  //   }
+  // }
 
   @Override
   public void periodic() {
