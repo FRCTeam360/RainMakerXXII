cb147633c0e6856192ddd985238caaea629006fb
tbmilliren <60276487+tbmilliren@users.noreply.github.com>
2022-02-09 16:23:40 -0800

    format shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -11,16 +11,11 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-
-
 import static frc.robot.Constants.ShooterConstants.*;
 import static frc.robot.Constants.CANIds.*;
 
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-
 
 public class Shooter extends SubsystemBase {
 
@@ -45,102 +40,102 @@ public class Shooter extends SubsystemBase {
   public double velocityTarget = 2000;
   public boolean isAtSpeed;
 
+  private Shooter() {
+    shooterLead = new CANSparkMax(shooterLeadId, MotorType.kBrushless);
+    shooterFollow = new CANSparkMax(shooterFollowId, MotorType.kBrushless);
 
-    private Shooter() {
-        shooterLead = new CANSparkMax(shooterLeadId, MotorType.kBrushless);
-        shooterFollow = new CANSparkMax(shooterFollowId, MotorType.kBrushless);
+    shooterPidController = shooterLead.getPIDController();
 
-        shooterPidController = shooterLead.getPIDController();
+    shooterEncoder = shooterLead.getEncoder();
 
-        shooterEncoder = shooterLead.getEncoder();
+    shooterLead.restoreFactoryDefaults();
+    shooterFollow.restoreFactoryDefaults();
 
-        shooterLead.restoreFactoryDefaults();
-        shooterFollow.restoreFactoryDefaults();
-    
-        shooterFollow.follow(shooterLead, true);
+    shooterFollow.follow(shooterLead, true);
 
-        shooterLead.setSmartCurrentLimit(40);
-        shooterFollow.setSmartCurrentLimit(40);
+    shooterLead.setSmartCurrentLimit(40);
+    shooterFollow.setSmartCurrentLimit(40);
 
-        shooterLead.setIdleMode(IdleMode.kCoast);
-        shooterFollow.setIdleMode(IdleMode.kCoast);
-    
-        shooterLead.setInverted(true);
-        // shooterFollow.setInverted(true);
-    
-        //shooterLead.setSensorPhase(true); //the Follower isn't harvested for it's encoder therefor rotation doesn't need to be modified
-       
-        // SmartDashboard.putNumber("kP", 0.0);
-        // SmartDashboard.putNumber("kI", 0.0);
-        // SmartDashboard.putNumber("kD", 0.0);
-        // SmartDashboard.putNumber("kF", 0.0);
+    shooterLead.setIdleMode(IdleMode.kCoast);
+    shooterFollow.setIdleMode(IdleMode.kCoast);
 
-        // set PID coefficients
+    shooterLead.setInverted(true);
+    // shooterFollow.setInverted(true);
 
-    }
+    // shooterLead.setSensorPhase(true); //the Follower isn't harvested for it's
+    // encoder therefor rotation doesn't need to be modified
+
+    // SmartDashboard.putNumber("kP", 0.0);
+    // SmartDashboard.putNumber("kI", 0.0);
+    // SmartDashboard.putNumber("kD", 0.0);
+    // SmartDashboard.putNumber("kF", 0.0);
+
+    // set PID coefficients
+
+  }
+
   /**
    * Gets the Singleton Shooter instance
+   * 
    * @return the Singleton Shooter instance
    */
-  public static Shooter getInstance(){
-    if(instance == null){
+  public static Shooter getInstance() {
+    if (instance == null) {
       instance = new Shooter();
     }
     return instance;
   }
 
-
-public double getVelocity(){
-  return shooterLead.getEncoder().getVelocity();
-}
-
-  @Override
-public void periodic() {
-  // kP = SmartDashboard.getNumber("kP", 0.0);
-  // kI = SmartDashboard.getNumber("kI", 0.0);
-  // kD = SmartDashboard.getNumber("kD", 0.0);
-  // kF = SmartDashboard.getNumber("kF", 0.0);
-
-  SmartDashboard.putNumber("Shooter Velocity", this.getVelocity());
-}
-
-/**
- * Sets shooter to percentage output
- * @param output motor output from -1 to 1
- */
-public void setSpeed(double output){
-  shooterLead.set(output);
-}
-
-/**
- * Sets shooter to velocity using PID and FeedForward
- * @param target target velocity in shooter RPMs
- */
-public void setVelocity(double target){
-
-  velocityTarget = target;
-
-  double error = velocityTarget - this.getVelocity();
-  
-  double deriv = velocityTarget - previousVelocity;
-  previousVelocity = this.getVelocity();
-  integral = integral + error;
-
-  double speed = (velocityTarget / kF) + (error * kP) + (integral * kI) - (deriv * kD);
-
-  //temporary limiting of max output - will probably change
-  speed = Math.min(speed, 0.7);
-  speed = Math.max(speed, -0.7);
-  
-  this.setSpeed(speed);
-  SmartDashboard.putNumber("speed set", speed);
+  public double getVelocity() {
+    return shooterLead.getEncoder().getVelocity();
   }
 
-  
-  public boolean isAtSpeed(){
+  @Override
+  public void periodic() {
+    // kP = SmartDashboard.getNumber("kP", 0.0);
+    // kI = SmartDashboard.getNumber("kI", 0.0);
+    // kD = SmartDashboard.getNumber("kD", 0.0);
+    // kF = SmartDashboard.getNumber("kF", 0.0);
+
+    SmartDashboard.putNumber("Shooter Velocity", this.getVelocity());
+  }
+
+  /**
+   * Sets shooter to percentage output
+   * 
+   * @param output motor output from -1 to 1
+   */
+  public void setSpeed(double output) {
+    shooterLead.set(output);
+  }
+
+  /**
+   * Sets shooter to velocity using PID and FeedForward
+   * 
+   * @param target target velocity in shooter RPMs
+   */
+  public void setVelocity(double target) {
+
+    velocityTarget = target;
+
+    double error = velocityTarget - this.getVelocity();
+
+    double deriv = velocityTarget - previousVelocity;
+    previousVelocity = this.getVelocity();
+    integral = integral + error;
+
+    double speed = (velocityTarget / kF) + (error * kP) + (integral * kI) - (deriv * kD);
+
+    // temporary limiting of max output - will probably change
+    speed = Math.min(speed, 0.7);
+    speed = Math.max(speed, -0.7);
+
+    this.setSpeed(speed);
+    SmartDashboard.putNumber("speed set", speed);
+  }
+
+  public boolean isAtSpeed() {
     double error = velocityTarget - this.getVelocity();
     return Math.abs(error) <= 100;
   }
 }
-
-
