2831a76dc4f36209a45effc9549a1471c3a7a8a8
juliashimshock <24shimshockjulia@bprep.org>
2022-03-16 16:29:22 -0700

    Update FieldOrientedDrive.java

--- src/main/java/frc/robot/commands/FieldOrientedDrive.java
+++ src/main/java/frc/robot/commands/FieldOrientedDrive.java
@@ -67,7 +67,6 @@ public class FieldOrientedDrive extends CommandBase {
     // field oriented drive conversion. forward = robot-based forward value, right =
     // robot-based turning adjustment
     double forward = upDownSquared * Math.cos(gyroRadians) + rightLeftSquared * Math.sin(gyroRadians);
-   
     double right = -1 * upDownSquared * Math.sin(gyroRadians) + rightLeftSquared * Math.cos(gyroRadians);
 
     // System.out.println("forward: " + forward);
